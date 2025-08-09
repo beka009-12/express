97dcf6cfec2f6e32fb6e13f634daee2873b10a33
beka009-12 <bekbolnurmsmitov220099@gmail.com>
2025-08-09 16:34:10 +0600

    .

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,12 @@ import { Router } from "express";
 import todoRoutes from "../modules/todo/todo.routes";
 import cors from "cors";
 
+const configCors = {
+  origin: ["http://localhost:3000"],
+};
+
 const router = Router();
 
-router.use("/todo", todoRoutes);
+router.use("/todo", cors(configCors), todoRoutes);
 
 export default router;
