08e60fb003937cec14cbffa891deb5d6a0731b65
beka009-12 <bekbolnurmsmitov220099@gmail.com>
2025-08-09 17:58:24 +0600

    .

--- src/modules/todo/todo.controllers.ts
+++ src/modules/todo/todo.controllers.ts
@@ -36,7 +36,6 @@ const addTodo = async (req: Request, res: Response) => {
         image ||
         "https://t3.ftcdn.net/jpg/04/60/01/36/360_F_460013622_6xF8uN6ubMvLx0tAJECBHfKPoNOR5cRa.jpg",
       description: description || "",
-      completed: false,
     };
     data.push(newTodo);
     res.status(201).send({
@@ -81,8 +80,7 @@ const deletetodo = async (req: Request, res: Response) => {
 const updateTodo = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
-    const { title, description, name, age, iamge, completed } = req.body;
-
+    const { title, description, name, age, image } = req.body;
     const todoIndex = data.findIndex((todo) => todo.id === +id);
     if (todoIndex === -1) {
       res.status(404).send({
@@ -96,18 +94,17 @@ const updateTodo = async (req: Request, res: Response) => {
     if (description !== undefined) data[todoIndex].description = description;
     if (name !== undefined) data[todoIndex].name = name;
     if (age !== undefined) data[todoIndex].age = age;
-    if (completed !== undefined) data[todoIndex].completed = completed;
-    if (iamge !== undefined) data[todoIndex].image = iamge;
+    if (image !== undefined) data[todoIndex].image = image;
+
     res.status(200).send({
       success: true,
-      message: "Todo updated successfully",
       data: data[todoIndex],
     });
-  } catch (e) {
-    console.error(`Error in updateTodo: ${e}`);
+  } catch (error) {
+    console.log(error);
     res.status(500).send({
       success: false,
-      message: `Error in updateTodo: ${e}`,
+      message: "Error updating todo",
     });
   }
 };
