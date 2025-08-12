ec1e587159ea4c2a588496c23faabe795f4c0d52
beka009-12 <bekbolnurmsmitov220099@gmail.com>
2025-08-12 10:04:42 +0600

    .

--- src/modules/todo-prisma/todo.controllers.ts
+++ src/modules/todo-prisma/todo.controllers.ts
@@ -14,4 +14,26 @@ const getTodos = async (req: Request, res: Response) => {
   }
 };
 
-export default { getTodos };
+const createTodo = async (req: Request, res: Response) => {
+  try {
+    const data = await prisma.todo.create({
+      data: {
+        image: "",
+        description: "",
+        title: "",
+      },
+    });
+    res.status(201).send({
+      status: "success",
+      data: data,
+    });
+  } catch (error) {
+    console.error(`Error in createTodo: ${error}`);
+    res.status(500).send({
+      success: false,
+      message: `Error in createTodo status 500: ${error}`,
+    });
+  }
+};
+
+export default { getTodos, createTodo };
